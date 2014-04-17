56e4a97b5cfde539d7d6cf5d4e3012c818d75964
Matt Harrison <mt.harrison86@gmail.com>
2014-04-17 08:41:23 +0100

    Refactoring and more tests

--- test/algos.js
+++ test/algos.js
@@ -6,7 +6,7 @@ describe('Hashologist', function(){
 
     it('Should be able to guess an MD5', function(){
       var hashologist = new Hashologist('d41d8cd98f00b204e9800998ecf8427e');
-      assert.equal(hashologist.solve()[0], "MD5");
+      assert.equal(hashologist.solve()[0].name, "MD5");
     });
 
   });
--- test/basic.js
+++ test/basic.js
@@ -4,7 +4,7 @@ var assert      = require("assert")
 describe('Hashologist', function(){
   describe('Basic features', function(){
 
-    it('Should be able to accept a hash in a constructor', function(){
+    it('Should accept a hash in a constructor', function(){
       var hashologist = new Hashologist('d41d8cd98f00b204e9800998ecf8427e');
       assert.equal(hashologist.hash, 'd41d8cd98f00b204e9800998ecf8427e');
     });
@@ -15,21 +15,21 @@ describe('Hashologist', function(){
         );
     });
 
-    it('Should be able to determine if a hash is alphabetic', function(){
+    it('Should determine if a hash is alphabetic', function(){
       var hashologist = new Hashologist('d41d8cd98f00b204e9800998ecf8427e');
       assert(hashologist.isAlphabetic());
       var hashologist = new Hashologist('1213548434');
       assert(!hashologist.isAlphabetic());
     });
 
-    it('Should be able to determine if a hash is numeric', function(){
+    it('Should determine if a hash is numeric', function(){
       var hashologist = new Hashologist('1213548434');
       assert(hashologist.isNumeric());
       var hashologist = new Hashologist('dsjglkgjhdfsjh');
       assert(!hashologist.isNumeric());
     });
 
-    it('Should be able to determine if a hash is alphanumeric', function(){
+    it('Should determine if a hash is alphanumeric', function(){
       var hashologist = new Hashologist('1213548434');
       assert(!hashologist.isAlphaNumeric());
       var hashologist = new Hashologist('dsjglkgjhdfsjh');
@@ -41,6 +41,54 @@ describe('Hashologist', function(){
     it('Should solve a hash, returning an array of likely hashes', function(){
       var hashologist = new Hashologist('1213548434');
       assert.equal(typeof hashologist.solve(), 'object');
+    });
+
+    it('Should test an algo predicate', function(){
+      var hashologist = new Hashologist('1234');
+      assert(hashologist.testPredicate('isNumeric', true)     === true);
+      assert(hashologist.testPredicate('isAlphabetic', false) === true);
+    });
+
+    it('Should throw for an invalid algo predicate', function(){
+      var hashologist = new Hashologist('1234');
+      assert.throws(function(){ hashologist.testPredicate('isFunky', true) });
+    });
+
+    it('Should test a set of algo predicates', function(){
+      var hashologist = new Hashologist('1234');
+
+      var algo1 = {
+        predicates: {
+          "isNumeric": true,
+          "length": 4
+        }
+      };
+      assert(hashologist.testAlgo(algo1) === true);
+
+      var algo2 = {
+        predicates: {
+          "isAlphaNumeric": true,
+          "length": 4
+        }
+      };
+      assert(hashologist.testAlgo(algo2) === false);
+
+      var algo3 = {
+        predicates: {
+          "isAlphaNumeric": false,
+          "length": 5
+        }
+      };
+      assert(hashologist.testAlgo(algo3) === false);
+
+      var algo4 = {
+        predicates: {
+          "isNumeric": false,
+          "length": 4
+        }
+      };
+      assert(hashologist.testAlgo(algo4) === false);
+
     });
 
   });
